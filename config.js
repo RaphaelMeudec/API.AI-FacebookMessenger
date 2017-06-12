a69f15a1555033e215f965aeb58b1a757e6f85b6
raph <raphael.meudec@gmail.com>
2017-06-12 19:26:25 +0200

    Add temporary tokens to test the subscription

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {}
 config.apiaitoken = 'apiai token'
 
 config.facebook = {
-    verification_token : 'Your verification token',
-    pageAccessToken : 'You page access token'
+    verification_token : '123456789123',
+    pageAccessToken : 'EAAN8EsO9Rw8BAP82TwshYWerjgbtXQE1qIf4IZBC7ZBsY68CqHZB9GtpdPuaVys3lJkMq3ZChq9QeX1ZB3q8OPiR9ZCZAyQneQQekaETY8EyKrlDWPVZAKvVm8sOJkMfHZAHwv9HdhFeHRotvody4P3NvWGqtWaSqvcVp8xbTdq9aOwZDZD'
 }
 
 module.exports = config
