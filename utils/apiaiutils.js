b2d16aba7045d2c0d1a5efd4936849b8567fb0c5
raph <raphael.meudec@gmail.com>
2017-06-12 19:55:44 +0200

    Add one case in handled fulfillments

--- utils/apiaiutils.js
+++ utils/apiaiutils.js
@@ -1,5 +1,10 @@
-
-
+function formatApiaiResponse(speech, displayText, source="heroku") {
+    return {
+        "speech": speech,
+        "displayText": displayText,
+        "source": source
+    }
+}
 
 
 var fulfillmentRequest = function(request, response) {
@@ -10,9 +15,12 @@ var fulfillmentRequest = function(request, response) {
         console.log('Valid fulfillment request received.');
         var action = body.result.action;
         var parameters = body.result.parameters;
-        // switch(body.result.action) {
-        //
-        // }
+        switch(body.result.action) {
+            case 'guess.name':
+                var json = formatApiaiResponse(speech='Your name is Bill.', displayText='Your name is Bill.')
+                response.json(json);
+                break;
+        }
     }
 }
 
